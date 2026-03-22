abe59a81d5423cb7dc74e24ff0c1dcee8c53c7a7
Andrew MacDonald <mail@ajmd.dev>
2026-03-22 19:09:54 -0300

    Console/Content browser UI rework ctd.

--- Source/Engine/editor/managed/Views/MainWindow.axaml.cs
+++ Source/Engine/editor/managed/Views/MainWindow.axaml.cs
@@ -16,11 +16,14 @@ namespace Hyperion.Editor
         private const bool CappedFrameRate = true;
         private const bool IsRenderingOnMainThread = true;
 
-        private ToggleButton? _toggleContentBrowser;
-        private ToggleButton? _toggleConsole;
         private Grid? _bottomPanelGrid;
+        private GridSplitter? _bottomPanelSplitter;
         private Control? _contentBrowserPanel;
+        private Border? _contentBrowserCollapsedStrip;
         private Control? _consolePanel;
+        private Border? _consoleCollapsedStrip;
+        private bool _contentBrowserExpanded = true;
+        private bool _consoleExpanded = true;
 
         public MainWindow()
         {
@@ -38,16 +41,22 @@ namespace Hyperion.Editor
 
             DataContext = new MainWindowViewModel();
 
-            _toggleContentBrowser = this.FindControl<ToggleButton>("ToggleContentBrowser");
-            _toggleConsole = this.FindControl<ToggleButton>("ToggleConsole");
             _bottomPanelGrid = this.FindControl<Grid>("BottomPanelGrid");
+            _bottomPanelSplitter = this.FindControl<GridSplitter>("BottomPanelSplitter");
             _contentBrowserPanel = this.FindControl<Control>("ContentBrowserPanel");
+            _contentBrowserCollapsedStrip = this.FindControl<Border>("ContentBrowserCollapsedStrip");
             _consolePanel = this.FindControl<Control>("ConsolePanel");
+            _consoleCollapsedStrip = this.FindControl<Border>("ConsoleCollapsedStrip");
 
-            if (_toggleContentBrowser != null)
-                _toggleContentBrowser.IsCheckedChanged += OnBottomPanelToggleChanged;
-            if (_toggleConsole != null)
-                _toggleConsole.IsCheckedChanged += OnBottomPanelToggleChanged;
+            var collapseContentBrowser = this.FindControl<Button>("CollapseContentBrowser");
+            var expandContentBrowser = this.FindControl<Button>("ExpandContentBrowser");
+            var collapseConsole = this.FindControl<Button>("CollapseConsole");
+            var expandConsole = this.FindControl<Button>("ExpandConsole");
+
+            if (collapseContentBrowser != null) collapseContentBrowser.Click += OnCollapseContentBrowser;
+            if (expandContentBrowser != null) expandContentBrowser.Click += OnExpandContentBrowser;
+            if (collapseConsole != null) collapseConsole.Click += OnCollapseConsole;
+            if (expandConsole != null) expandConsole.Click += OnExpandConsole;
 
             if (IsRenderingOnMainThread)
             {
@@ -59,22 +68,27 @@ namespace Hyperion.Editor
             }
         }
 
-        private void OnBottomPanelToggleChanged(object? sender, RoutedEventArgs e)
+        private void OnCollapseContentBrowser(object? sender, RoutedEventArgs e) { _contentBrowserExpanded = false; UpdateBottomPanelLayout(); }
+        private void OnExpandContentBrowser(object? sender, RoutedEventArgs e) { _contentBrowserExpanded = true; UpdateBottomPanelLayout(); }
+        private void OnCollapseConsole(object? sender, RoutedEventArgs e) { _consoleExpanded = false; UpdateBottomPanelLayout(); }
+        private void OnExpandConsole(object? sender, RoutedEventArgs e) { _consoleExpanded = true; UpdateBottomPanelLayout(); }
+
+        private void UpdateBottomPanelLayout()
         {
             if (_bottomPanelGrid == null) return;
 
-            bool showContent = _toggleContentBrowser?.IsChecked == true;
-            bool showConsole = _toggleConsole?.IsChecked == true;
-
-            if (_contentBrowserPanel != null)
-                _contentBrowserPanel.IsVisible = showContent;
-            if (_consolePanel != null)
-                _consolePanel.IsVisible = showConsole;
-
             var cols = _bottomPanelGrid.ColumnDefinitions;
-            cols[0].Width = showContent ? new GridLength(1, GridUnitType.Star) : new GridLength(0);
-            cols[1].Width = (showContent && showConsole) ? new GridLength(2) : new GridLength(0);
-            cols[2].Width = showConsole ? new GridLength(1, GridUnitType.Star) : new GridLength(0);
+            cols[0].Width = _contentBrowserExpanded ? new GridLength(1, GridUnitType.Star) : new GridLength(30);
+            cols[1].Width = (!_contentBrowserExpanded && !_consoleExpanded) ? new GridLength(0) : new GridLength(2);
+            cols[2].Width = _consoleExpanded ? new GridLength(1, GridUnitType.Star) : new GridLength(30);
+
+             bool bothExpanded = _contentBrowserExpanded && _consoleExpanded;
+            if (_bottomPanelSplitter != null) _bottomPanelSplitter.IsEnabled = bothExpanded;
+
+            if (_contentBrowserPanel != null) _contentBrowserPanel.IsVisible = _contentBrowserExpanded;
+            if (_contentBrowserCollapsedStrip != null) _contentBrowserCollapsedStrip.IsVisible = !_contentBrowserExpanded;
+            if (_consolePanel != null) _consolePanel.IsVisible = _consoleExpanded;
+            if (_consoleCollapsedStrip != null) _consoleCollapsedStrip.IsVisible = !_consoleExpanded;
         }
 
         // need to destroy the engine window when MainWindow is closed
